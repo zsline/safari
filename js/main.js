1ee18f24b042aa3f71726b65f278b9ad65e25242
zsline <sergei.dereka@gmal.com>
2022-01-17 21:46:51 +0200

    premium

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $(function(){
-
+  $('.popup__link').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+  });
+  $('.galery__item-inner').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function (item) {
+        return item.el.attr('title') + '<small>Safari Africa</small>';
+      }
+    }
+  });
+  $('.galery__slider').slick({
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev slick-arrows"><img src="./images/prev.svg" alt="" /></button>',
+    nextArrow: '<button type="button" class="slick-next slick-arrows"><img src="./images/next.svg" alt="" /></button>',
+  });
 });
